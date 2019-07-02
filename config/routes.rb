d4396a747617f1fe50831a33888bffd1d37d971a
Vicentecorrea <vicorrea@miuandes.cl>
2019-07-02 15:05:14 -0400

    RESTful (using request in Postman)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admins
   resources :admins
-  resources :transactions
-  resources :users
+  # resources :transactions
+  defaults format: :json do
+    resources :transactions
+  end
+  # resources :users
+  defaults format: :json do
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
