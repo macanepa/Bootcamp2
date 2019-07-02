31006930b7160922d6fd5f470979e059188f0b10
Matias Canepa <macanepa@miuandes.cl>
2019-07-02 17:27:46 -0400

    heroku pg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!
 
   # GET /users
   # GET /users.json
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @transactions=Transaction.where(user_id: @user.id)
   end
 
   # GET /users/new
