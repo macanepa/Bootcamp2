193c6ed2e4638e17163e07e9699d6428aa5c6297
Matias Canepa <macanepa@miuandes.cl>
2019-07-03 01:17:28 -0400

    removed authenticate admin from users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_admin!
+  # before_action :authenticate_admin!
 
   # GET /users
   # GET /users.json
