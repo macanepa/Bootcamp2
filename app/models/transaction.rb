7efd718115d61d88481fc560d415fa3cd1cd3bba
Matias Canepa <macanepa@miuandes.cl>
2019-07-03 03:14:09 -0400

    Transaction Validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,27 @@
 class Transaction < ApplicationRecord
   belongs_to :user
+
+  # validates :amount, numericality: { other_than: 0 }
+  validate :check_balance
+
+  after_create :discount_balance
+
+  def check_balance
+    puts("\n\n\n\n\n\nYeah!!!!!\n\n\n\n\n\n")
+    balance = User.find(user_id).balance
+    puts("Balance:#{balance} Amount:#amount}")
+    if amount < 0
+      if amount.abs > balance
+        errors.add(:amount, "Ho lee fuk")
+      end
+    end
+  end
+
+
+  def discount_balance
+    user = User.find(user_id)
+    user.balance += amount
+    user.save
+  end
+
 end
